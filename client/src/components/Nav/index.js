cc570a212eba451e5f08dc2aaafaf915ee165632
LMBoyle <boylelukem@gmail.com>
2019-11-18 11:12:35 -0500

    master

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,18 @@
+// Imports ========================================================================================
+
+import React from "react";
+import "./style.css"
+
+// Functions ======================================================================================
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-dark bg-dark justify-content-center">
+      <a href="/" className="navbar-brand navbar-dark"> Cheapo </a>
+    </nav>
+  );
+}
+
+// Export =========================================================================================
+
+export default Nav;
---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,28 @@
+// Imports ========================================================================================
+
+import React from "react";
+
+// Components
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+
+
+// Functions ======================================================================================
+
+function NoMatch() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-12">
+          <Jumbotron>
+            <h1> Dashboard </h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+// Export =========================================================================================
+
+export default NoMatch;
