eb164f2c30daab13a0280871ade7e59f29cec6b7
Krish <aalsbhowmick@gmail.com>
2019-11-23 13:43:49 -0500

    user profile page

---
+++ client/src/pages/App/Profile.js
@@ -0,0 +1,28 @@
+// Imports ========================================================================================
+
+import React from "react";
+
+// Components
+import { Col, Row, Container } from "../../components/Grid";
+import Jumbotron from "../../components/Jumbotron";
+
+
+// Functions ======================================================================================
+
+function Profile() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-12">
+          <Jumbotron>
+            <h1> User Profile</h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+// Export =========================================================================================
+
+export default Profile;
