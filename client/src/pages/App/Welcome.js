15c8452fccd5c8d3959082378d6d3947dd9e4be7
LMBoyle <boylelukem@gmail.com>
2019-12-07 11:59:26 -0500

    added animation to welcome page

--- client/src/pages/App/Welcome.js
+++ client/src/pages/App/Welcome.js
@@ -1,6 +1,8 @@
 // Imports ========================================================================================
 
-import React from "react";
+import React, { Component } from "react";
+import Rotate from "react-reveal/Rotate";
+
 import "./Pages.css"
 
 // Components
@@ -15,64 +17,72 @@ const friendsMoney = require("../../assets/img/friendsMoney.jpg")
 
 // Functions ======================================================================================
 
-function Welcome() {
-  return (
-    <div className="container my-0">
-      <Card
-        cardClass={"cardWrap"}
-        styleCard={{minHeight: 1028}}
-        styleBody={{padding: "0 0 50px 0"}}
-      >
-        <Row>
-          <Col size="md-12">
-            <Jumbotron
-              style={{color: "#650D1B"}}
-            >
-              <h1> Welcome to Cheapo! </h1>
-              <p> We strive to help you help your friends save money! </p>
-            </Jumbotron>
-          </Col>
-        </Row>
-        <Container>
+class Welcome extends Component {
+  
+
+  render() {
+    return (
+      <div className="container my-0">
+        <Card
+          cardClass={"cardWrap"}
+          styleCard={{minHeight: 1028}}
+          styleBody={{padding: "0 0 50px 0"}}
+        >
           <Row>
-            <Col size="md-6">
-              <CardImg
-                src={savingJars}
-                alt="Jars full of change with labels for different saving goals"
-              />
-            </Col>
-            <Col size="md-6">
-              <CardHeader
-                title="Save Money"
-                styleHeader={{backgroundColor: "#BF7E04"}}
-                styleCard={{textAlign: "center", top: "3rem"}}
+            <Col size="md-12">
+              <Jumbotron
+                style={{color: "#650D1B"}}
               >
-                Save towards rent, vacation or treating yourself to something nice!
-              </CardHeader>
+                <h1> Welcome to Cheapo! </h1>
+                <p> We strive to help you help your friends save money! </p>
+              </Jumbotron>
             </Col>
           </Row>
-          <Row>
-            <Col size="md-6">
-              <CardHeader
-                title="Connect With Friends"
-                styleHeader={{backgroundColor: "#BF7E04"}}
-                styleCard={{textAlign: "center", top: "7rem"}}
-              >
-                See how close friends are to meeting their goals and cheer them on!
-              </CardHeader>
-            </Col>
-            <Col size="md-6">
-              <CardImg
-                src={friendsMoney}
-                alt="Group of friends reaching for falling money"
-              />
-            </Col>
-          </Row>
-        </Container>
-      </Card>
-    </div>
-  );
-}
+          <Container>
+            <Row>
+              <Col size="md-6">
+                <Rotate top left>
+                  <CardImg
+                    src={savingJars}
+                    alt="Jars full of change with labels for different saving goals"
+                  />
+                </Rotate>
+              </Col>
+              <Col size="md-6">
+                <CardHeader
+                  title="Save Money"
+                  styleHeader={{backgroundColor: "#BF7E04"}}
+                  styleCard={{textAlign: "center", top: "3rem"}}
+                >
+                  Save towards rent, vacation or treating yourself to something nice!
+                </CardHeader>
+              </Col>
+            </Row>
+            <Row>
+              <Col size="md-6">
+                <CardHeader
+                  title="Connect With Friends"
+                  styleHeader={{backgroundColor: "#BF7E04"}}
+                  styleCard={{textAlign: "center", top: "7rem"}}
+                >
+                  See how close friends are to meeting their goals and cheer them on!
+                </CardHeader>
+              </Col>
+              <Col size="md-6">
+                <Rotate top right>
+                  <CardImg
+                    src={friendsMoney}
+                    alt="Group of friends reaching for falling money"
+                  />
+                </Rotate>
+              </Col>
+            </Row>
+          </Container>
+        </Card>
+      </div>
+    );
+  };
+};
 
 // Export =========================================================================================
 
